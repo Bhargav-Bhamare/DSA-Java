63084dc84269628574afe68668b2a54027bedc5e
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-04-16 09:48:40 +0530

    Solved Hollow Rectangle Problem

---
+++ hRectangle.java
@@ -0,0 +1,20 @@
+class hRectangle {
+    public static void hollow_Rectangle(int totalRows, int totalCols){
+        //outer loop
+        for(int i =1;i<=totalRows;i++){
+            //inner loop
+            for(int j = 1;j<=totalCols;j++){
+                //cell - (i-j)
+                if( i == 1 || i == totalRows || j == 1 || j == totalCols){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hollow_Rectangle(24,28);
+    }
+}
