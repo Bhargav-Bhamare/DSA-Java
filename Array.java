05d84e852a05d97341fdf5edc255444c333f7afe
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-04-17 22:10:25 +0530

    Practicing Array insertion

--- Array.java
+++ Array.java
@@ -11,3 +11,19 @@ class Array{
         System.out.println("Maths Marks = "+ marks[2]);
     }
 }
+
+class Array{
+    public static void updateMarks(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+1;
+        }
+    }
+    public static void main(String[] args){
+        int marks[]= {98,99,95};
+        updateMarks(marks);
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]+" ");
+        }
+        System.out.println();
+    }
+}
