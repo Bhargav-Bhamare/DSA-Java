97dc9281b9d801482c5b7916c92f9ac9e734a8c7
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-05-30 10:09:46 +0530

    Solved Few String Related Problems

---
+++ palindromeProb.java
@@ -0,0 +1,16 @@
+public class palindromeProb {
+
+    public static boolean checkPalindrome( String str){
+        int n = str.length();
+        for(int i = 0;i< n/2;i++){
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "bhargav";
+        System.out.println(checkPalindrome(str));
+    }
+}
