1838daba11d71c397f22d96038984a013e6383d2
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-07-10 10:27:44 +0530

    Solving Recursion Problems

--- Recursion/occurence.java
+++ Recursion/occurence.java
@@ -10,8 +10,20 @@ public class occurence {
         }
         return firstOccurence(arr, key, i+1);
     }
+
+    public static int lastOccurence(int arr[], int key, int i){
+        if(i == arr.length-1){
+            return -1;
+        }
+        //Look Forward
+        int isFound = lastOccurence(arr, key, i+1);
+        if( isFound == -1 && arr[i] == key){
+            return i;
+        }
+        return isFound;
+    }
     public static void main(String[] args) {
         int arr[] = {2,4,5,7,2,5,9};
-        System.out.println(firstOccurence(arr, 5, 0));
+        System.out.println(lastOccurence(arr, 5, 0));
     }
 }
---
+++ Recursion/power.java
@@ -0,0 +1,31 @@
+public class power {
+
+    //Time Complexity = O(n)---Too high,therefore we go towards Optimized approach
+    public static int calcPower(int x, int n){
+        if( n == 0){
+            return 1;
+        }
+        int X = x * calcPower(x, n-1);
+        return X;
+    }
+
+    public static int optimizedPower(int a, int n){
+        if(n == 0){
+            return 1;
+        }
+
+        int halfPower = optimizedPower(a, n/2);
+        int halfPowerSq = halfPower * halfPower;
+        //Odd
+        if( n % 2 != 0){
+            halfPowerSq = a * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(optimizedPower(2, 10));
+    }
+}
