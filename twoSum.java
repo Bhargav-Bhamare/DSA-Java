14863fe0b868788c3370474f9ea49c01f6c0a575
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-04 20:48:43 +0530

    Practicing Something

--- twoSum.java
+++ twoSum.java
@@ -1,21 +1,19 @@
 public class twoSum {
     
-    public static int twoSumProb(int[] nums, int target) {
+    public static void twoSumProb(int[] nums, int target) {
         for(int i =0;i<nums.length;i++){
             int currSum =i;
             for(int j=i+1;j<nums.length;j++){
                 currSum+=j;
                 if(currSum == target){
-                    System.out.println("sol found!");
-                    return 1;
+                    System.out.println("Index of the two numbers are: "+i+" and "+j);
                 }
             }
         }
-        return 0;
     }
     public static void main(String[] args) {
         int nums[] = {3,2,4};
         int target = 6;
-        System.out.println(twoSumProb(nums,target));
+        twoSumProb(nums,target);
     }
 }
