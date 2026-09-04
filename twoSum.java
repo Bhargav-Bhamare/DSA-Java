04526202c4f3aa77e69b517602a1a421b837d888
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-04 22:39:08 +0530

    Done with this problem!!

--- twoSum.java
+++ twoSum.java
@@ -2,9 +2,9 @@ public class twoSum {
     
     public static void twoSumProb(int[] nums, int target) {
         for(int i =0;i<nums.length;i++){
-            int currSum =i;
+            int currSum =nums[i];
             for(int j=i+1;j<nums.length;j++){
-                currSum+=j;
+                currSum+=nums[j];
                 if(currSum == target){
                     System.out.println("Index of the two numbers are: "+i+" and "+j);
                 }
@@ -12,8 +12,8 @@ public class twoSum {
         }
     }
     public static void main(String[] args) {
-        int nums[] = {3,2,4};
-        int target = 6;
+        int nums[] = {2,7,11,15};
+        int target = 9;
         twoSumProb(nums,target);
     }
 }
