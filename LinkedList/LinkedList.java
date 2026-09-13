08e33640f1b60fc6255593534b6c24421c74b51a
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-13 20:55:03 +0530

    Practicing Linked List Basics

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -46,6 +46,25 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx -1){
+            temp = temp.next;
+            i++;
+        }
+
+        // i = idx-1..,i.e temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
 
     public static void main(String[] args) {
        LinkedList ll = new LinkedList();
