941f36e52a536b4a0f3d0a8342f4f61ef5ddfa15
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-14 08:37:25 +0530

    Practicing Linked List Basics

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -126,6 +126,21 @@ public class LinkedList {
         return -1;
     }
 
+    public int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+
+        return idx+1;
+    }
+
 
     public static void main(String[] args) {
        LinkedList ll = new LinkedList();
