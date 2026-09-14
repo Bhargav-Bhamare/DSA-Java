50b8cf21339b43a8c52f106c180a9e8ff31d94c4
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-14 08:07:09 +0530

    Practicing Linked List Basics

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -69,6 +69,20 @@ public class LinkedList {
         temp.next = newNode;
     }
 
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is Empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;  //IMP LINE
+        return val;
+    }
+
 
     public static void main(String[] args) {
        LinkedList ll = new LinkedList();
