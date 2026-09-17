5f20c1d1f99a48767c87bb55779028d4cc207f28
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-17 07:42:47 +0530

    Practicing Linked List Basics

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -243,6 +243,47 @@ public class LinkedList {
         prev.next = null;
     }
 
+    private Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return mergedLL.next; //Next bcoz first Node is -1
+    }
+
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        return slow; //Mid Node
+    }
+
     //Merge Sort on LL
     public Node mergeSort(Node head){
         if(head == null || head.next == null){
