8357d5521ca6d87e09b752634fc6bb54b96dee34
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-18 06:54:28 +0530

    Practicing Linked List Zig Zag Pattern

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -302,6 +302,46 @@ public class LinkedList {
         return merge(newLeft, newRight);
     }
 
+    //Zig zag Pattern
+    public void zigZag(){
+        //Find Mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        //Reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        //Alternate Merge - Zig Zag Merge
+        while(left != null && right != null){
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
 
     public static void main(String[] args) {
        LinkedList ll = new LinkedList();
