2576a496c6d0c8b1d6eae846a62fc42a538b6254
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-18 07:18:37 +0530

    Practicing Doubly Linked List Basics

--- LinkedList/DoublyLL.java
+++ LinkedList/DoublyLL.java
@@ -59,6 +59,24 @@ public class DoublyLL {
         return val;
     }
 
+    //Reverse Linked List
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
     public static void main(String[] args) {
         DoublyLL dll = new DoublyLL();
         dll.addFirst(3);
