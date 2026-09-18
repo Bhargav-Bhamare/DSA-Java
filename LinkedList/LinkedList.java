bcdfd31d8c9fe946b2d6cc0719a93befe734be6d
Bhargav Bhamare <bbhamare9e20@gmail.com>
2026-09-18 08:16:16 +0530

    Practicing Palindrome of Linked List Problem

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -188,6 +188,45 @@ public class LinkedList {
         return;
     }
 
+    //Palindrome Linked List
+    private Node findMid(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean checkPalindrome(){
+        if(head == null || head.next == null){
+            return true;
+        }
+        //Find Middle
+        Node mid = findMid(head);
+
+        //Reverse 2nd half
+        Node curr = mid;
+        Node prev = null;
+        while(curr != null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        while(right != null){
+            if(left.data != right.data){
+                return false;
+            }
+            left =left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
     //Detecting cycle in a linked list
     public boolean hasCycle(Node head) {
         Node fast = head;
